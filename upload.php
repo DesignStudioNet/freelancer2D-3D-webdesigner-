59c9b531a95670bbe354377d783f4928a6afe1a3
DesignStudioNet <164730280+DesignStudioNet@users.noreply.github.com>
2024-04-23 20:21:01 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "uploads/";
+$target_dir = "upload/";
 $target_file = $target_dir .
 basename($_FILES["fileToUpload"]["name"]);
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {echo "Le fichier" .
